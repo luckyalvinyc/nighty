b5bb1ed5c0be605277208270aa3d5031cc3d2daa
Lucky Alvin Y. Casipe <luckyalvinyc@gmail.com>
2023-04-13 15:00:43 +0800

    Setup `simplecov` gem

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,10 @@
+require "simplecov"
+
+SimpleCov.start "rails" do
+  enable_coverage :branch
+  primary_coverage :branch
+end
+
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
