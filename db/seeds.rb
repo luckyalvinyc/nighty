e996c15973e442d1385288011bf748cb85f49cc7
Lucky Alvin Y. Casipe <luckyalvinyc@gmail.com>
2023-04-14 08:56:02 +0800

    Update seed for adding users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "\n###"
+puts "# Adding users"
+puts "#"
+
+User.upsert_all([
+  {
+    id: 1,
+    name: 'lucky'
+  },
+  {
+    id: 2,
+    name: 'alvin'
+  },
+  {
+    id: 3,
+    name: 'john'
+  },
+  {
+    id: 4,
+    name: 'doe'
+  }
+])
+
+puts "\n###"
+puts "# Done adding #{User.count} users"
+puts "#"
